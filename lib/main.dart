99938b20442ac7114d6058c13a6e7e50b58d576b
Harshang Virani <52438740+harshangvirani@users.noreply.github.com>
2026-03-27 09:34:25 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,41 @@ import 'core/theme/app_theme.dart';
 //   runApp(const ProviderScope(child: WeAdsApp()));
 // }
 
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:we_ads/core/providers/flavor_provider.dart';
+import 'main.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Pre-load the font to prevent the "Bad state" crash on cold start
+  // try {
+  //   final fontData = await rootBundle.load('assets/fonts/Roboto-Regular.ttf');
+  //   final loader = FontLoader('Roboto');
+  //   loader.addFont(Future.value(fontData));
+  //   await loader.load();
+  //   debugPrint("Font 'Roboto' loaded successfully");
+  // } catch (e) {
+  //   debugPrint("Font loading failed: $e");
+  // }
+  runApp(
+    ProviderScope(
+      overrides: [
+        flavorProvider.overrideWithValue(
+          FlavorConfig(
+            flavor: 'dev',
+            apiBaseUrl:
+                'http://weaddswebapi-env.eba-4minhvmy.us-west-1.elasticbeanstalk.com/',
+          ),
+        ),
+      ],
+      child: const WeAdsApp(),
+    ),
+  );
+}
+
 class WeAdsApp extends ConsumerWidget {
   const WeAdsApp({super.key});
 
